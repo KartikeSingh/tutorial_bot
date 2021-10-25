f863956f16de16a327e82c8c5133d0cef3c11ba7
KrazyDev <67275060+KartikeSingh@users.noreply.github.com>
2021-10-25 12:53:32 +0530

    Episode 10 | Pokemon 🦚

--- src/commands/admin/pokemon.js
+++ src/commands/admin/pokemon.js
@@ -3,21 +3,19 @@ const guildConfig = require('../../models/guildConfig');
 module.exports = {
     data: {
         name: "pokemon",
-        description: "Manage pokemon game for your server",
+        description: "Setup the pokemon game for your server",
         options: [{
             name: "enable-spawn",
             type: 1,
-            description: "Enable pokemon spawning",
-            options: []
+            description: "Enable the slash commands"
         }, {
             name: "disable-spawn",
             type: 1,
-            description: "Disable pokemon spawning",
-            options: []
+            description: "Disable the slash commands"
         }, {
             name: "spawn-after",
             type: 1,
-            description: "Random pokemon spawn's required points",
+            description: "Random pokemon spawning points",
             options: [{
                 name: "number",
                 type: 4,
@@ -34,41 +32,44 @@ module.exports = {
                 description: "Mention or give ID, 0 for same channel spawn",
                 required: true
             }]
-        }]
+        }],
     },
-    permissions: ["MANAGE_SERVER", "MANAGE_ROLES"],
+    permissions: ["MANAGE_SERVER"],
 
-    /**
-     * 
-     * @param {Client} client 
-     * @param {CommandInteraction} interaction 
-     */
     run: async (client, interaction) => {
-        await interaction.reply({ content: `${client.user.username} is thinking...` });
+        await interaction.deferReply();
         const data = await guildConfig.findOne({ id: interaction.guildId }) || await guildConfig.create({ id: interaction.guildId });
         const command = interaction.options.getSubcommand();
 
         if (command === "enable-spawn") {
-            if (data.pokemon.spawn) return interaction.editReply({ content: "The pokemon spawn is already enabled" });
+            if (data.pokemon.spawn) return interaction.editReply({ content: `The pokemon spawning is already enabled` });
+
             await guildConfig.findOneAndUpdate({ id: interaction.guildId }, { "pokemon.spawn": true });
 
             interaction.editReply({ content: "Enabled pokemon spawning" });
         } else if (command === "disable-spawn") {
-            if (!data.pokemon.spawn) return interaction.editReply({ content: "The pokemon spawn is already disabled" });
+            if (data.pokemon.spawn) return interaction.editReply({ content: `The pokemon spawning is already disabled` });
+
             await guildConfig.findOneAndUpdate({ id: interaction.guildId }, { "pokemon.spawn": false });
 
-            interaction.editReply({ content: "Disabled pokemon spawning" });
+            interaction.editReply({ content: "disabled pokemon spawning" });
         } else if (command === "spawn-after") {
-            await guildConfig.findOneAndUpdate({ id: interaction.guildId }, { "pokemon.afterPoints": interaction.options.getInteger("number") });
+            const points = interaction.options.getInteger("number");
 
-            interaction.editReply({ content: `Pokemon spawning required points are not setted to ${interaction.options.getInteger("number")}` });
+            if (points < 10) return interaction.editReply({ content: "The points can't be below 10" });
+
+            await guildConfig.findOneAndUpdate({ id: interaction.guildId }, { "pokemon.afterPoints": points });
+
+            interaction.editReply({ content: `Pokemon spawning required points are not setted to ${points}` });
         } else if (command === "spawn-channel") {
-            let raw = interaction.options.getString("channel"), channel = raw === "0" ? raw : interaction.guild.channels.cache.get(raw) || interaction.guild.channels.cache.get(raw.substring(2, raw.length - 1));
+            const raw = interaction.options.getString("channel"), channel = raw === "0" ? raw : interaction.guild.channels.cache.get(raw) || interaction.guild.channels.cache.get(raw.substring(2, raw.length - 1));
+
 
             if (!channel || (channel.type !== "GUILD_TEXT" && channel.type !== "GUILD_NEWS")) return interaction.editReply("Invalid channel was provided, either type 0 for same channel, or mention / give id of a text channel")
-            await guildConfig.findOneAndUpdate({ id: interaction.guildId }, { "pokemon.spawnAt": channel });
+            await guildConfig.findOneAndUpdate({ id: interaction.guildId }, { "pokemon.spawnAt": channel === "0" ? "0" : channel.id });
 
             interaction.editReply({ content: `${channel === "0" ? "Setted the pokemon spawning to the same channel where latest message was sent" : `Now pokemons will spawn it <#${channel.id}>`}` });
         }
+
     }
 }
--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,47 +1,45 @@
-const configs = require('../models/guildConfig');
-const pokecord = require('pokecord');
+const guildConfigs = require('../models/guildConfig');
 const users = require('../models/userConfig');
-const { MessageEmbed, Message } = require('discord.js');
+const pokecord = require('pokecord');
+const { MessageEmbed } = require('discord.js');
 
-/**
- * 
- * @param {*} client 
- * @param {Message} message 
- * @returns 
- */
 module.exports = async (client, message) => {
     if (message.author.bot) return;
 
-    const data = await configs.findOne({ id: message.guild.id });
+    const data = await guildConfigs.findOne({ id: message.guild.id });
 
-    if (!data || !data.pokemon.spawn || Date.now() - data.pokemon.lastMessage < 5000) return;
-    data.pokemon.points += Math.floor(Math.random() * 3 + 1);
+    if (!data || !data.pokemon.spawn) return;
 
-    if (data.pokemon.points < data.pokemon.afterPoints) return configs.findOneAndUpdate({ id: message.guild.id }, { "pokemon.points": data.pokemon.points, "pokemon,lastMessage": Date.now() });
-    await configs.findOneAndUpdate({ id: message.guild.id }, { "pokemon.points": 0, "pokemon,lastMessage": Date.now() });
+    data.pokemon.points += Math.floor(Math.random() * 3) + 1;
+
+    if (data.pokemon.points < data.pokemon.afterPoints) return guildConfigs.findOneAndUpdate({ id: message.guild.id }, { "pokemon.points": data.pokemon.points });
 
     const pokemon = await pokecord.Spawn();
+
+    const channel = message.guild.channels.cache.get(data.pokemon.spawnAt) || message.channel;
+
     const embed = new MessageEmbed()
         .setTitle("A new pokemon has appeared")
         .setImage(pokemon.imageURL)
         .setDescription(`Catch pokemon by typing \`catch < pokemon name >\``);
 
-    const msg = await message.channel.send({ embeds: [embed] });
+    const msg = await channel.send({ embeds: [embed] });
 
-    message.channel.awaitMessages({
+    msg.channel.awaitMessages({
         time: 60000,
         errors: ['time'],
         filter: (m) => m.content.toLowerCase() === `catch ${pokemon.name.toLowerCase()}`,
         max: 1
-    }).then(async v => {
-        console.log(v.first())
-        await users.findOneAndUpdate({ user: v.first().author.id }, { $push: { pokemons: pokemon.id } }) || await users.create({
-            user: v.first().author.id,
+    }).then(async col => {
+        const msg = col.first();
+
+        await users.findOneAndUpdate({ user: msg.author.id }, { $push: { pokemons: pokemon.id } }) || await users.create({
+            user: msg.author.id,
             pokemons: [pokemon.id]
         });
 
-        v.first().reply(`You successfully caught \`${pokemon.name}\` pokemon`);
-    }).catch(e => {
+        msg.reply(`You successfully caught \`${pokemon.name}\` pokemon`);
+    }).catch(() => {
         msg.reply(`No one replied with correct name & syntax, So the pokemon is gone.\nThe name of the pokemon was : \`${pokemon.name}\``);
     })
 }
